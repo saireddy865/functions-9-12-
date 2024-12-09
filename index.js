8514cc46262bc0cf7aacc2942343e43b9c1324db
saireddy865-github <saireddymuppidi28@gmail.com>
2024-12-09 19:41:20 +0530

    done

---
+++ index.js
@@ -0,0 +1,64 @@
+// ---------  1st answer -------
+
+function greet(){
+    console.log("Hello!");
+}
+greet();
+
+
+console.log(" -------------- ");
+
+
+// ----- 2nd answer ---------
+
+
+function showMessage(){
+    console.log("This is a message !");
+}
+showMessage();
+
+console.log(" ---------------- ");
+
+
+
+// ---------- 3rd answer ---------
+
+
+function logInfo(){
+    console.log("logging Information");
+}
+logInfo();
+
+console.log(" -------------- ");
+
+
+// ---------- 4th answer ----------
+
+
+function startGame(){
+    console.log("Game Started !");
+    endGame();
+}
+function endGame(){
+    console.log("Game Over !");
+}
+startGame();
+endGame();
+
+
+console.log(" -------------- ");
+
+
+
+// --------- 5th answer ---------
+
+
+function beginTask(){
+    console.log("Task Started");
+    finishTask();
+}
+function finishTask(){
+    console.log("Task Finished");
+}
+beginTask();
+finishTask();
